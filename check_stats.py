05ffedfd2bbbebb72c4ef6077b024699ccb9392c
LinusWemmer <linus.wemmer@mailbox.tu-dresden.de>
2025-02-24 17:09:29 +0100

    fixed small thin

--- check_stats.py
+++ check_stats.py
@@ -11,9 +11,4 @@ if __name__ == "__main__":
     gen1 = Fillomino_Generator(size, max_region)
     times1 = gen1.generate_stats()
     average1 = statistics.mean(times1)
-
-    gen2 = Fillomino_Generator_Alt(size, max_region)
-    times2 = gen1.generate_stats()
-    average2 = statistics.mean(times2)
     print("Old Method Average Time: " + str(average1))
-    print("New Method Average Time: " + str(average2))
