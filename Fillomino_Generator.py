73e44769ecbd0106bbfbda29e192c8cf9fe992ef
LinusWemmer <linus.wemmer@mailbox.tu-dresden.de>
2025-03-20 17:18:04 +0100

    reworked solving to work with async and maximize

--- Fillomino_Generator.py
+++ Fillomino_Generator.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import re
 import clingo 
+import time
 
 
 class Fillomino_Generator:
@@ -36,17 +37,21 @@ class Fillomino_Generator:
         self.current_program = solution_string
 
     def store_puzzle(self, model):
+        print("bla")
         sym_seq =  model.symbols(shown=True)
         self.current_puzzle = []
         self.current_program = ""
+        removed_cells = f"{self.step}: "
         for atom in sym_seq:
             if atom.name == "removed":
-                self.solution_steps.append([s.number for s in atom.arguments])
+                removed_cells += str(([s.number for s in atom.arguments]))
+                self.step += 1
             elif "blocked" in atom.name:
-                self.solution_steps.append("b")
+                removed_cells +="b"
             else:
                 self.current_program += str(atom).replace("remaining", "fillomino") + ". "
                 self.current_puzzle.append(atom)
+        self.solution_steps.append(removed_cells)
 
     def generate_fillomino(self):
         # No randomization is needed; a different Fillomino is generated every time
@@ -72,53 +77,67 @@ class Fillomino_Generator:
     def generate_puzzle(self):
         #TODO: probably a subset of the program should be preground to avoid recomputation, e.g. pos and the adjacency predicate
         #Though this only constitutes a small part of the computation time
+        #TODO: probably add heuristic to prioritize numbers with few neighbours, to avoid early splitting of regions
         satisfiable = True
-        step = 1
-        solver = open("logic_programs/expand_area.lp", "r")
-        expand_area = solver.read()
-        solver.close()
+        self.step = 1
+        expand_area_string = open("logic_programs/expand_area.lp", "r")
+        expand_area = expand_area_string.read()
+        expand_area_string.close()
+        enter_one_string = open("logic_programs/expand_area.lp", "r")
+        enter_one = enter_one_string.read()
+        enter_one_string.close()
         while satisfiable:
-            print(step)
             self.ctl = clingo.Control(arguments=["-t 8", "--stats"])
             self.ctl.add("base", [], self.current_program)
             self.ctl.ground([("base",[])])
             self.ctl.add("base", ["n", "k"], expand_area)
             self.ctl.ground([("base",[self.length, self.max_region])])
-            if self.ctl.solve(on_model=self.store_puzzle).unsatisfiable:
-                satisfiable = False  
-            step +=1
+            result = False
+            with self.ctl.solve(yield_=True) as handle:
+                result = handle.get()
+                if result.unsatisfiable:
+                    satisfiable = False
+                else:
+                    self.store_puzzle(handle.model())
+            #if self.ctl.solve(on_finish=self.store_puzzle, yield_=True).unsatisfiable:
+            #    satisfiable = False  
+            #self.step +=1
+            print(self.step)
         print(self.solution_steps)
         return self.current_puzzle
     
-    def generate_puzzle_maximize(self, n: int):
-        solver = open("logic_programs/expand_area.lp", "r")
-        expand_area = solver.read()
-        solver.close()
-        puzzle_steps = []
-        puzzle_list = []
-        for i in range(0,n):
-            self.solution_steps = []
-            satisfiable = True
-            self.current_program = self.solution_program
-            step = 1
-            while satisfiable:
-                print(step)
-                self.ctl = clingo.Control(arguments=["-t 8", "--stats"])
-                self.ctl.add("base", [], self.current_program)
-                self.ctl.ground([("base",[])])
+
+    # Multiple Tries to generate an efficient puzzle
+    # TODO: we should probably start at a different place in the search area, as of now multiple tries can generate the same puzzle
+  #  def generate_puzzle_maximize(self, n: int):
+   #     solver = open("logic_programs/expand_area.lp", "r")
+    #    expand_area = solver.read()
+     #   solver.close()
+      #  puzzle_steps = []
+       # puzzle_list = []
+        #for i in range(0,n):
+         #   self.solution_steps = []
+          #  satisfiable = True
+           # self.current_program = self.solution_program
+            #step = 1
+            #while satisfiable:
+             #   print(step)
+              #  self.ctl = clingo.Control(arguments=["-t 8", "--stats"])
+               # self.ctl.add("base", [], self.current_program)
+                #self.ctl.ground([("base",[])])
                 
-                self.ctl.add("base", ["n", "k"], expand_area)
-                self.ctl.ground([("base",[self.length, self.max_region])])
-                if self.ctl.solve(on_model=self.store_puzzle).unsatisfiable:
-                    satisfiable = False  
-                    puzzle_steps.append(step)
-                    puzzle_list.append(self.current_puzzle)
-                    self.current_program = self.solution_fillomino
-                step +=1
-            print(self.solution_steps)
-        max_index = puzzle_list.index(max(puzzle_list))
-        print(puzzle_steps)
-        return puzzle_list[max_index]
-        return self.current_puzzle
+                #self.ctl.add("base", ["n", "k"], expand_area)
+               # self.ctl.ground([("base",[self.length, self.max_region])])
+              #  if self.ctl.solve(on_model=self.store_puzzle(step)).unsatisfiable:
+             #       satisfiable = False  
+            #        puzzle_steps.append(step)
+           #         puzzle_list.append(self.current_puzzle)
+         #           self.current_program = self.solution_fillomino
+          #      step +=1
+        #    print(self.solution_steps)
+       # max_index = puzzle_list.index(max(puzzle_list))
+      #  print(puzzle_steps)
+     #   return puzzle_list[max_index]
+     #   return self.current_puzzle
     
 
