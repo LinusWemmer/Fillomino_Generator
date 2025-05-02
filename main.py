da984c8d598ade42b993f9ef65d1196eb257c0a9
LinusWemmer <linus.wemmer@mailbox.tu-dresden.de>
2025-05-02 17:09:43 +0200

    added functionality to generate pzpr links using the javascript api

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import clingo 
+import subprocess
+import json
 from Fillomino_Generator import *
+
 
 def on_model(model):
     print(model)
@@ -12,24 +15,26 @@ def print_fillomino(gen_model, size: int):
     fillomino = [[0 for _ in range(int(size))] for _ in range(int(size))]
     for item in fillomino_list:
         cell = item.arguments
-        #cell = re.findall(r"\(.*\)", item)[0][1:-1].split(",")
         cell = [s.number for s in cell]
         fillomino[cell[0] -1][cell[1] - 1] = cell[2]
     for row in fillomino:
         print(row)
     print("")
+    return fillomino
 
 
 if __name__ == "__main__":
-    #size = input("Size: ")
-    #max_region = input("Maximum Region: ")
+    # size = input("Size: ")
+    # max_region = input("Maximum Region: ")
     # This works quite reliably for size up to 7 (and works with 8)
-    #For quicker testing for now:
-    size = 7
-    max_region = 5
-    
+    # For quicker testing for now:
+    size = 5
+    max_region = 6
+
     gen = Fillomino_Generator(size, max_region)
     gen_model = gen.generate_fillomino()
     print_fillomino(gen_model, size)
-    print_fillomino(gen.generate_puzzle(), size)
+    puzzle = print_fillomino(gen.generate_puzzle(), size)   
+    result = subprocess.run(['node', 'bla.js', json.dumps(puzzle)], capture_output=True, text=True)
+    print(result.stdout)
 
---
+++ puzzle.js
@@ -0,0 +1,20 @@
+var pzpr = require('pzpr');
+
+var arg = process.argv[2];
+const grid = JSON.parse(arg);
+var puzzle = new pzpr.Puzzle();
+puzzle.open(`fillomino/${grid.length}/${grid.length}`)
+for (let i = 0; i < grid.length; i++) {
+    row = grid[i];
+    console.log(row);
+    for (let j=0; j < row.length; j++){
+        number = row[j];
+        if (number != 0) {
+            cx = 2 * i + 1;
+            cy = 2 * j + 1;
+            puzzle.board.getc(cy,cx).qnum = number;
+        }
+    }   
+}
+
+console.log(puzzle.getURL());
