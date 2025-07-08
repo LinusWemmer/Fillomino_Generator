847d2595a9e09077abc49682527e700e092b019f
LinusWemmer <linus.wemmer@mailbox.tu-dresden.de>
2025-07-08 14:32:14 +0200

    changed the printing after cleaning up the code

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import clingo
 import subprocess
 import json
 import time
+import re
 from Fillomino_Generator import *
 
 
@@ -10,13 +11,10 @@ def on_model(model):
 clingo.SymbolType
 
 def print_fillomino(gen_model, size: int):
-    #solved = open(file, "r")
-    #fillomino_string = solved.read()
     fillomino_list = gen_model
     fillomino = [[0 for _ in range(int(size))] for _ in range(int(size))]
     for item in fillomino_list:
-        cell = item.arguments
-        cell = [s.number for s in cell]
+        cell = [int(x) for x in re.findall(r'\d+', item)]
         fillomino[cell[0] -1][cell[1] - 1] = cell[2]
     for row in fillomino:
         print(row)
@@ -29,14 +27,15 @@ if __name__ == "__main__":
     # max_region = input("Maximum Region: ")
     # For quicker testing for now:
     size = 5
-    largest_region = 5
+    largest_region = 7
     max_regions = 25
     start_time = time.time()
     print("Starting!\n")
     gen = Fillomino_Generator(size, largest_region, max_regions)
-    gen_model = gen.generate_fillomino()
-    print_fillomino(gen_model, size)
+    gen_model_str = gen.generate_fillomino()
+    print_fillomino(gen_model_str, size)
     puzzle = print_fillomino(gen.generate_puzzle_naive(), size)   
+    gen.get_human_solvable_puzzle()
     result = subprocess.run(['node', 'puzzle.js', json.dumps(puzzle)], capture_output=True, text=True)
     print(result.stdout)
     print(f"Total Computation Time:{time.time()-start_time}")
