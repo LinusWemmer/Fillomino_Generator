676c704b81f56e665f1e93da473eabe65ecf615f
LinusWemmer <linus.wemmer@mailbox.tu-dresden.de>
2025-07-29 19:02:38 +0200

    some cleanup (not really cleaner)

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import subprocess
 import json
 import time
 import re
+import copy
 from Fillomino_Generator import *
 
 
@@ -23,20 +24,28 @@ def print_fillomino(gen_model, size: int):
 
 
 if __name__ == "__main__":
-    # size = input("Size: ")
-    # max_region = input("Maximum Region: ")
+    print("Generate Fillominos with ASP...\n")
+    size = input("Size: ")
+    largest_region = input("Maximum Region: ")
     # For quicker testing for now:
-    size = 5
-    largest_region = 7
+    if size == "":
+        size = 5
+    if largest_region == "":
+        largest_region = 7
     max_regions = 25
     start_time = time.time()
     print("Starting!\n")
-    gen = Fillomino_Generator(size, largest_region, max_regions)
+    gen = Fillomino_Generator(int(size), int(largest_region), max_regions)
     gen_model_str = gen.generate_fillomino()
     print_fillomino(gen_model_str, size)
-    gen.generate_puzzle_naive()
-    puzzle = print_fillomino(gen.get_human_solvable_puzzle(), size)   
+    puzzle = print_fillomino(gen.generate_puzzle(), int(size))
+    #gen.generate_puzzle_naive()
+    #copy = copy.deepcopy(gen)
+    #puzzle = print_fillomino(gen.get_human_solvable_puzzle(options="min"), size)   
+    #puzzle_2 = print_fillomino(copy.get_human_solvable_puzzle(), size)
     result = subprocess.run(['node', 'puzzle.js', json.dumps(puzzle)], capture_output=True, text=True)
     print(result.stdout)
+    #result = subprocess.run(['node', 'puzzle.js', json.dumps(puzzle_2)], capture_output=True, text=True)
+    #print(result.stdout)
     print(f"Total Computation Time:{time.time()-start_time}")
 
