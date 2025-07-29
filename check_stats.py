2ed3d543011c7f9f3d8e207a9ef6f984ab086229
LinusWemmer <linus.wemmer@mailbox.tu-dresden.de>
2025-07-29 19:03:08 +0200

    added histogram creation for checking some statistics of generated fillominos

--- check_stats.py
+++ check_stats.py
@@ -9,13 +9,13 @@ def most_frequent(List):
     occurence_count = Counter(List)
     return occurence_count.most_common(1)[0][0]
 
-if __name__ == "__main__":
+def check_naive(iterations: int):
     size = 5
     largest_region = 7
     max_regions = 25
-    removals = [8,9,9,10,11,9,8,12,9,8,10,8,8,8,8,8,8]
+    removals = []
     gen = Fillomino_Generator(size, largest_region, max_regions)
-    for i in range(0,10):
+    for i in range(0,iterations):
         gen.generate_fillomino()
         size = len(gen.generate_puzzle_naive())
         removals.append(size)
@@ -27,3 +27,25 @@ if __name__ == "__main__":
     plt.ylabel('Frequency')
     plt.yticks((range(0,max_count + 1)))
     plt.show()
+
+def check_top_down(iterations:int):
+    size = 6
+    largest_region = 7
+    max_regions = 25
+    removals = []
+    gen = Fillomino_Generator(size, largest_region, max_regions)
+    for i in range(0,iterations):
+        gen.generate_fillomino()
+        size = len(gen.generate_puzzle())
+        removals.append(size)
+    plt.hist(removals, bins=range(min(removals), max(removals) + 2), align='left', rwidth=0.8, color='skyblue', edgecolor='black')
+    max_count = removals.count(most_frequent(removals)) 
+
+    plt.xlabel('Remaining')
+    plt.xticks(range(min(removals), max(removals) + 1))
+    plt.ylabel('Frequency')
+    plt.yticks((range(0,max_count + 1)))
+    plt.show()
+
+if __name__ == "__main__":
+    check_top_down(50)
